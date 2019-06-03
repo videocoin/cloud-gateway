5b450f3135d9c7a613a6692dc48299cb920ad391
earl <earl@liveplanet.net>
2019-06-03 14:35:45 -0700

    Add default verifer and manager rpc ports

--- service/config.go
+++ service/config.go
@@ -10,6 +10,8 @@ type Config struct {
 	UsersRpcAddr     string `default:"0.0.0.0:5000"`
 	AccountsRpcAddr  string `default:"0.0.0.0:5001"`
 	PipelinesRpcAddr string `default:"0.0.0.0:5002"`
+	ManagerRpcAddr   string `default:"0.0.0.0:50051"`
+	VerifierRpcAddr  string `default:"0.0.0.0:50055"`
 
 	Logger *logrus.Entry `envconfig:"-"`
 }
