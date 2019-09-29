ec90cf70b57b7cb7cbecd6fe2b3079d920709c66
Nikolay Stoev <nikolay@liveplanet.net>
2019-09-28 20:18:44 -0700

    add profiles service

--- service/config.go
+++ service/config.go
@@ -6,9 +6,10 @@ type Config struct {
 	Name    string `envconfig:"-"`
 	Version string `envconfig:"-"`
 
-	Addr           string `default:"0.0.0.0:8080"`
-	UsersRpcAddr   string `default:"0.0.0.0:5000"`
-	StreamsRpcAddr string `default:"0.0.0.0:5002"`
+	Addr            string `default:"0.0.0.0:8080"`
+	UsersRpcAddr    string `default:"0.0.0.0:5000"`
+	StreamsRpcAddr  string `default:"0.0.0.0:5002"`
+	ProfilesRpcAddr string `default:"0.0.0.0:5004"`
 
 	Logger *logrus.Entry `envconfig:"-"`
 }
