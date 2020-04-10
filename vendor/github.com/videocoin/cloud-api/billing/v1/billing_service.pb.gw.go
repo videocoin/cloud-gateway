12179042f170d4178e740917c10a9f2d5218d518
Aleksandr Didenko <aa.didenko@yandex.com>
2020-04-10 14:07:45 +0300

    Update deps

--- vendor/github.com/videocoin/cloud-api/billing/v1/billing_service.pb.gw.go
+++ vendor/github.com/videocoin/cloud-api/billing/v1/billing_service.pb.gw.go
@@ -120,6 +120,24 @@ func local_request_BillingService_GetTransactions_0(ctx context.Context, marshal
 
 }
 
+func request_BillingService_GetChartCharges_0(ctx context.Context, marshaler runtime.Marshaler, client BillingServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq types.Empty
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.GetChartCharges(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_BillingService_GetChartCharges_0(ctx context.Context, marshaler runtime.Marshaler, server BillingServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq types.Empty
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.GetChartCharges(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterBillingServiceHandlerServer registers the http handlers for service BillingService to "mux".
 // UnaryRPC     :call BillingServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -202,6 +220,26 @@ func RegisterBillingServiceHandlerServer(ctx context.Context, mux *runtime.Serve
 		}
 
 		forward_BillingService_GetTransactions_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_BillingService_GetChartCharges_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_BillingService_GetChartCharges_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_BillingService_GetChartCharges_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -326,6 +364,26 @@ func RegisterBillingServiceHandlerClient(ctx context.Context, mux *runtime.Serve
 
 	})
 
+	mux.Handle("GET", pattern_BillingService_GetChartCharges_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_BillingService_GetChartCharges_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_BillingService_GetChartCharges_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -337,6 +395,8 @@ var (
 	pattern_BillingService_GetCharges_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "billing", "charges"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_BillingService_GetTransactions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "billing", "transactions"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_BillingService_GetChartCharges_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "v1", "billing", "charts", "charges"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -347,4 +407,6 @@ var (
 	forward_BillingService_GetCharges_0 = runtime.ForwardResponseMessage
 
 	forward_BillingService_GetTransactions_0 = runtime.ForwardResponseMessage
+
+	forward_BillingService_GetChartCharges_0 = runtime.ForwardResponseMessage
 )
